7bb994c091967165ddeea964b8fd02ed863b5a3d
효정이 <w2029@e-mirim.hs.kr>
2021-07-21 14:34:34 +0900

    Edit ResultActivity.java Commit

--- app/src/main/java/kr/hs/emirim/w2029/project_bmi/ResultActivity.java
+++ app/src/main/java/kr/hs/emirim/w2029/project_bmi/ResultActivity.java
@@ -2,7 +2,10 @@ package kr.hs.emirim.w2029.project_bmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -10,5 +13,36 @@ public class ResultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        ImageView imgV = findViewById(R.id.imgv);
+        TextView text1 = findViewById(R.id.text_result1);
+        TextView text2 = findViewById(R.id.text_result2);
+
+        Intent intent = getIntent();
+        double w = intent.getDoubleExtra("weight", 0);
+        double h = intent.getDoubleExtra("height", 0);
+        // Bmi 단위 : kg/m'(치중 / 키의 제곱)
+        double bmi = w / Math.pow(h/100,2);
+
+        String bmiStr = "";
+        int imgSrcId = 0;
+
+        if(bmi < 18.5){
+            bmiStr = "저";
+            imgSrcId = R.drawable.bmi_1;
+        }else if(bmi < 24){
+            bmiStr = "정상";
+            imgSrcId = R.drawable.bmi_2;
+        }else if(bmi < 30){
+            bmiStr = "과체중";
+            imgSrcId = R.drawable.bmi_3;
+        }else if(bmi < 37){
+            bmiStr = "비만";
+            imgSrcId = R.drawable.bmi_4;
+        }else {
+            bmiStr = "초고도비만";
+            imgSrcId = R.drawable.bmi_5;
+        }
+        imgV.setImageResource(imgSrcId);
+        text1.setText(bmiStr + "체중");
     }
 }
